04d0ddcd5494a4a686963f773926dd73152f5737
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-04-24 23:24:20 +0300

    refactored

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 
 
@@ -8,7 +9,7 @@ export default class CreateExercise extends Component {
     constructor(props) {
         super(props);
 
-        this.onChangeUsernameme = this.onChangeUsername.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
         this.onChangeDescription = this.onChangeDescription.bind(this);
         this.onChangeDuration = this.onChangeDuration.bind(this);
         this.onChangeDate = this.onChangeDate.bind(this);
@@ -24,10 +25,15 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -64,6 +70,10 @@ export default class CreateExercise extends Component {
             date: this.state.date
         }
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercise/add', exercise)
+            .then(res => console.log(res.data));
+
         window.location = '/';
     }
 
