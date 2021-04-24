0d041df6fc687e34d2164ddabd5ddfff87e58ce6
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-04-24 16:05:10 +0300

    created schemas

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     User.find()
-    .then(users => res.join(users))
+    .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
