d5be711110c6cd7f3a0ee4fdd3734c2e3e40da52
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-04-25 00:32:40 +0300

    finishing

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -71,7 +71,7 @@ export default class CreateExercise extends Component {
         }
         console.log(exercise);
 
-        axios.post('http://localhost:5000/exercise/add', exercise)
+        axios.post('http://localhost:5000/exercises/add', exercise)
             .then(res => console.log(res.data));
 
         window.location = '/';
